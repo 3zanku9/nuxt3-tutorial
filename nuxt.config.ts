4cf2ffece6732ef59ef180b4b4e55712a17f7e86
takahiro <fuchi@jriss.jp>
2022-11-09 11:02:47 +0900

    my first commmit

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    postcss: {
+        plugins: { tailwindcss: {} }
+    },
+    css: ['~/assets/css/tailwind.css'],
+    modules: ['@nuxtjs/tailwindcss'],
+})
